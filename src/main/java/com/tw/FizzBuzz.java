e958fa24d98e19f57f4a12e8c23106412bab1e6c
puwang <puwang@thoughtworks.com>
2017-07-16 23:14:43 +0800

    remove the FizzBuzzMatcher

--- src/main/java/com/tw/FizzBuzz.java
+++ src/main/java/com/tw/FizzBuzz.java
@@ -7,19 +7,19 @@ import java.util.List;
 public class FizzBuzz {
     private final BuzzMatcher buzzMatcher = new BuzzMatcher();
     private final FizzMatcher fizzMatcher = new FizzMatcher();
-    private final FizzBuzzMatcher fizzBuzzMatcher = new FizzBuzzMatcher(fizzMatcher, buzzMatcher);
-    private List<Matcher> matchers = Arrays.asList(buzzMatcher, fizzBuzzMatcher, fizzMatcher);
+    private List<Matcher> matchers = Arrays.asList(buzzMatcher, fizzMatcher);
 
     public FizzBuzz() {
         Collections.sort(matchers);
     }
 
     public String count(int number) {
+        String result = "";
         for (Matcher matcher : matchers) {
             if (matcher.isMatched(number)) {
-                return matcher.getText();
+                result += matcher.getText();
             }
         }
-        return String.valueOf(number);
+        return result.isEmpty() ? String.valueOf(number) : result;
     }
 }
